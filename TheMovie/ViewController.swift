eba5dc0c160cdb2059fab530884e37ce3f2b456c
herlanggawibi <wibiherlanggga24@gmail.com>
2024-08-08 18:51:23 +0700

    Initial Commit

---
+++ TheMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovie
+//
+//  Created by User on 08/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
